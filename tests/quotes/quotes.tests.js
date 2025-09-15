be2fc77799561b106d7fe5ea97f41ba83436e82c
Niels Drost <47660417+nielsdrost7@users.noreply.github.com>
2025-09-15 12:30:26 +0200

    quotes tests

--- tests/quotes/quotes.tests.js
+++ tests/quotes/quotes.tests.js
@@ -1,9 +1,9 @@
 const { page, expect } = require('jest-playwright-preset');
-const { assertPageLoads, assertDestroy, assertAjax } = require('../test-helpers');
+const { assertPageLoads, assertDestroy, assertAjax } = require('../../test-helpers');
 
 describe('Quotes Module', () => {
 
-  // Index Routes
+  // View Routes
   test('it can view quotes index', async () => {
     await assertPageLoads(page, '/quotes/index');
     await expect(page.locator('.content-title')).toContainText('Quotes');
@@ -12,36 +12,35 @@ describe('Quotes Module', () => {
   test('it can view all quotes', async () => {
     await assertPageLoads(page, '/quotes/status/all');
   });
-  
+
   test('it can view approved quotes', async () => {
     await assertPageLoads(page, '/quotes/status/approved');
   });
-  
+
   test('it can view canceled quotes', async () => {
     await assertPageLoads(page, '/quotes/status/canceled');
   });
-  
+
   test('it can view draft quotes', async () => {
     await assertPageLoads(page, '/quotes/status/draft');
   });
-  
+
   test('it can view rejected quotes', async () => {
     await assertPageLoads(page, '/quotes/status/rejected');
   });
-  
+
   test('it can view sent quotes', async () => {
     await assertPageLoads(page, '/quotes/status/sent');
   });
-  
+
   test('it can view viewed quotes', async () => {
     await assertPageLoads(page, '/quotes/status/viewed');
   });
 
-  // View Routes
-  test('it can view a quote', async () => {
-    await assertPageLoads(page, '/quotes/view/1');
+  test('it can view a quote by id', async () => {
+    await assertPageLoads(page, '/quotes/view/2172');
   });
-  
+
   // Destroy Routes
   test('it can delete a quote', async () => {
     await assertDestroy(page, '/quotes/delete/1');
@@ -50,9 +49,9 @@ describe('Quotes Module', () => {
   test('it can cancel a quote', async () => {
     await assertDestroy(page, '/quotes/cancel/1');
   });
-  
+
   // AJAX Routes
-  test('it can generate a quote PDF', async () => {
-    await assertAjax(page, '/quotes/generate_pdf/1');
+  test('it can generate a quote PDF by id', async () => {
+    await assertAjax(page, '/quotes/generate_pdf/2172');
   });
 });
