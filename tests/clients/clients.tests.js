209ef4d3a1aa4689d83d722bc138950af6af08d6
Niels Drost <47660417+nielsdrost7@users.noreply.github.com>
2025-09-15 12:01:15 +0200

    client tests

--- tests/clients/clients.tests.js
+++ tests/clients/clients.tests.js
@@ -1,10 +1,10 @@
 const { page, expect } = require('jest-playwright-preset');
-const { assertPageLoads, assertFormSubmit, assertDestroy } = require('../test-helpers');
+const { assertPageLoads, assertFormSubmit, assertDestroy } = require('../../test-helpers');
 
 describe('Clients Module', () => {
 
-  // Index Routes
-  test('it can view clients index page', async () => {
+  // View Routes
+  test('it can view clients index', async () => {
     await assertPageLoads(page, '/clients/index');
     await expect(page.locator('.content-title')).toContainText('Clients');
   });
@@ -21,30 +21,13 @@ describe('Clients Module', () => {
     await assertPageLoads(page, '/clients/status/inactive');
   });
 
-  test('it can view user clients index', async () => {
-    await assertPageLoads(page, '/user_clients/index');
-  });
-
-  // View Routes
-  test('it can view a client profile', async () => {
-    await assertPageLoads(page, '/clients/view/1');
+  test('it can view client profile by id', async () => {
+    await assertPageLoads(page, '/clients/view/837');
     await expect(page.locator('.content-title')).toContainText('Client Profile');
   });
 
-  test('it can view client invoices', async () => {
-    await assertPageLoads(page, '/clients/view/1/invoices');
-  });
-
-  test('it can view guest client profile', async () => {
-    await assertPageLoads(page, '/guest/view/1');
-  });
-
-  test('it can view guest invoice', async () => {
-    await assertPageLoads(page, '/guest/invoice/1');
-  });
-
-  test('it can view guest quote', async () => {
-    await assertPageLoads(page, '/guest/quote/1');
+  test('it can view client invoices by id', async () => {
+    await assertPageLoads(page, '/clients/view/837/invoices');
   });
 
   // Form Routes
@@ -52,16 +35,8 @@ describe('Clients Module', () => {
     await assertFormSubmit(page, '/clients/form', 'client');
   });
 
-  test('it can edit an existing client', async () => {
-    await assertFormSubmit(page, '/clients/form/1', 'client');
-  });
-
-  test('it can create a new user client', async () => {
-    await assertFormSubmit(page, '/user_clients/form', 'user_clients');
-  });
-
-  test('it can edit a user client', async () => {
-    await assertFormSubmit(page, '/user_clients/form/1', 'user_clients');
+  test('it can edit an existing client by id', async () => {
+    await assertFormSubmit(page, '/clients/form/956', 'client');
   });
 
   // Destroy Routes
@@ -72,9 +47,4 @@ describe('Clients Module', () => {
   test('it can remove a client', async () => {
     await assertDestroy(page, '/clients/remove/1');
   });
-
-  test('it can delete a user client', async () => {
-    await assertDestroy(page, '/user_clients/delete/1');
-  });
-
 });
