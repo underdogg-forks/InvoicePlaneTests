5649fb17af069ce2e09cabd591cf4e2f34814085
Niels Drost <47660417+nielsdrost7@users.noreply.github.com>
2025-09-15 12:24:31 +0200

    payments tests

--- tests/payments/payments.tests.js
+++ tests/payments/payments.tests.js
@@ -1,16 +1,12 @@
 const { page, expect } = require('jest-playwright-preset');
-const { assertPageLoads, assertFormSubmit, assertDestroy, assertAjax } = require('../test-helpers');
+const { assertPageLoads, assertFormSubmit, assertDestroy, assertAjax } = require('../../test-helpers');
 
 describe('Payments Module', () => {
 
-  // Index Routes
+  // View Routes
   test('it can view payments index', async () => {
     await assertPageLoads(page, '/payments/index');
     await expect(page.locator('.content-title')).toContainText('Payments');
-  });
-
-  test('it can view payment methods index', async () => {
-    await assertPageLoads(page, '/payment_methods/index');
   });
 
   // Form Routes
@@ -18,16 +14,8 @@ describe('Payments Module', () => {
     await assertFormSubmit(page, '/payments/form', 'payments');
   });
 
-  test('it can edit an existing payment', async () => {
-    await assertFormSubmit(page, '/payments/form/1', 'payments');
-  });
-
-  test('it can create a new payment method', async () => {
-    await assertFormSubmit(page, '/payment_methods/form', 'payments');
-  });
-
-  test('it can edit an existing payment method', async () => {
-    await assertFormSubmit(page, '/payment_methods/form/1', 'payments');
+  test('it can edit an existing payment by id', async () => {
+    await assertFormSubmit(page, '/payments/form/966', 'payments');
   });
 
   // Destroy Routes
@@ -35,13 +23,8 @@ describe('Payments Module', () => {
     await assertDestroy(page, '/payments/delete/1');
   });
 
-  test('it can delete a payment method', async () => {
-    await assertDestroy(page, '/payment_methods/delete/1');
-  });
-
   // AJAX Routes
   test('it can view online logs', async () => {
     await assertAjax(page, '/payments/online_logs');
   });
-
 });
